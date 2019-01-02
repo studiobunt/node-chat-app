aa4e5c7bcb3866976d7e11e3ca04314606ffc070
Dzenan <dzenan555@hotmail.com>
2019-01-02 22:24:15 +0300

    added js folder

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+var socket = io();
+socket.on('connect', function() {
+    console.log('connected to server');
+});
+
+socket.on('disconnect', function() {
+    console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function(message){
+    console.log('New message', message);
+});
