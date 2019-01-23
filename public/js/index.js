ff9ed9c3ccc4203dc3a5d1260586a3f979cecfc6
Dzenan <dzenan555@hotmail.com>
2019-01-23 22:17:35 +0300

    add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -26,11 +26,13 @@ socket.on('newLocationMessage', function(message){
 
 jQuery('#message-form').on('submit', function(e){
     e.preventDefault();
+
+    var messageTextbox = jQuery('[name=message]');
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function(){
-
+        messageTextbox.val('')
     });
 });
 
@@ -39,7 +41,9 @@ locationButton.on('click', function(){
     if(!navigator.geolocation){
         return alert('Geolocation not supported by your browser');
     }
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
     navigator.geolocation.getCurrentPosition(function(position){
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
